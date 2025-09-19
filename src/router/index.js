dbdb529b1b1b1c259ff38f8b8d5ffae44ebc39f4
ziye <2878592560@qq.com>
2025-09-19 18:51:41 +0800

    router update4

--- src/router/index.js
+++ src/router/index.js
@@ -3,7 +3,7 @@ import Home from '../components/Home.vue'
 import Timetable from '../components/Timetable.vue'
 
 const routes = [
-  { path: '/', redirect: '/CoursePage_of_ChinesePainting/home' },
+  { path: '/', redirect: '/home' },
   { path: '/home', name: 'Home', component: Home },
   { path: '/timetable', name: 'Timetable', component: Timetable },
 ]
